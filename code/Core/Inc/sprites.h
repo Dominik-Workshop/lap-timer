eefbf2240713374a3bdb588c1fa65c1423509dd9
Dominik Pluta <nmarcin87@gmail.com>
2025-12-06 11:47:18 +0100

    Remove unused sprites

--- code/Core/Inc/sprites.h
+++ code/Core/Inc/sprites.h
@@ -16,13 +16,4 @@
 
 extern const uint8_t *bitmap_lap_timer_logo_128_64;
 
-extern const uint8_t *bitmap_sneak100_128_64;
-extern const uint8_t *bitmap_konar_horizontal_128_64;
-extern const uint8_t *bitmap_konar_vertical_128_64;
-
-extern const uint8_t *bitmap_minecraft_night_vision_32_32;
-extern const uint8_t *bitmap_minecraft_eye_of_ender_32_32;
-extern const uint8_t *bitmap_minecraft_sword_32_32;
-extern const uint8_t *bitmap_minecraft_book_32_32;
-
 #endif
